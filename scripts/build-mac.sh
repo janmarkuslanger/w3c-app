7394d0fa4fdd0d8d6fd060e461925653c8666c85
Jan-Markus Langer <hallo@janmarkuslanger.de>
2019-01-02 18:49:22 +0100

    zip -> tar

--- scripts/build-mac.sh
+++ scripts/build-mac.sh
@@ -1,3 +1,3 @@
-eval 'electron-packager . mac --overwrite --platform=darwin --arch=x64 --prune=true --out=release-builds'
-eval 'zip release-builds/mac.zip -r release-builds/mac-darwin-x64'
+eval 'electron-packager . mac --overwrite --platform=darwin --arch=x64 --prune=true --out=release-builds --ignore='
+eval 'tar -zcvf release-builds/mac.tar.gz release-builds/mac-darwin-x64'
 eval 'rm -rf release-builds/mac-darwin-x64'
