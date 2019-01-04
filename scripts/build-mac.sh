a2ac0b99ebb1640e48d5d9e5fd03eb92ead4d50c
Jan-Markus Langer <hallo@janmarkuslanger.de>
2019-01-04 19:03:38 +0100

    add app icon

--- scripts/build-mac.sh
+++ scripts/build-mac.sh
@@ -1,7 +1,7 @@
 RELEASE_DIR="release-builds"
 APP_FOLDER="w3c-darwin-x64"
 
-eval "electron-packager . w3c --overwrite --platform=darwin --arch=x64 --prune=true --out=$RELEASE_DIR --ignore=assets"
+eval "electron-packager . w3c --overwrite --platform=darwin --arch=x64 --prune=true --icon=assets/icons/icon.icns --out=$RELEASE_DIR --ignore=assets"
 eval "cd $RELEASE_DIR/"
 eval "tar -zcvf mac.tar.gz $APP_FOLDER"
 eval "cd ../"
