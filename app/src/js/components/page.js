9c7ac7968aa54cca78680539cdec19c68f3863d2
Jan-Markus Langer <hallo@janmarkuslanger.de>
2019-09-15 13:27:16 +0200

    Add List item render methods to page class

--- app/src/js/components/page.js
+++ app/src/js/components/page.js
@@ -4,11 +4,33 @@ const axios = require('axios');
 class Page {
   constructor(url) {
     this.url = url;
+    this.messages = [];
+    this.renderListItem();
   }
 
   async fetchData() {
     return await axios.get(`https://validator.nu/?doc=${this.url}&out=json`);
   }
+
+  createListItem() {
+    return h('li', {class: 'list-group-item'}, [
+      h('div', {class: 'action-bar'}, [
+        h('div', {class: 'js--close'}, [
+          h('span', {class: 'icon icon-cancel'})
+        ])
+      ]),
+      h('p', {class: 'padded'}, [
+        h('strong', {class: 'url'}, [
+          this.url
+        ])
+      ])
+    ]);
+  }
+
+  renderListItem() {
+    document.querySelector('.list-group').appendChild(this.createListItem());
+  }
+
 };
 
 export default Page;
