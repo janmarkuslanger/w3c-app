35213df1eef0938f752b7f38cccf01cdce465d24
Jan-Markus Langer <hallo@janmarkuslanger.de>
2019-09-15 14:46:27 +0200

    Add a preloader on fetching sitemap data

--- app/src/js/components/sitemap.js
+++ app/src/js/components/sitemap.js
@@ -1,4 +1,7 @@
 import Page from './page';
+import isUrl from '../utils/is-url';
+const axios = require('axios');
+import { init as UiPreloaderInit } from 'ui-preloader';
 
 class Sitemap {
   constructor(url) {
@@ -18,15 +21,19 @@ class Sitemap {
 
   async fetchUrls() {
     const response = await axios.get(this.url);
-    this.urls = response.data.data
+    this.urls = response.data
       .match(/<loc>(.*?)<\/loc>/g)
       .map((link) => link.replace(/<\/?loc>/g,''));
   }
 
   async render() {
-    await fetchUrls();
+    const loader = UiPreloaderInit();
+    await this.fetchUrls();
     this.urls.forEach((url) => {
       new Page(url);
     });
+    loader.destroy();
   }
 }
+
+export default Sitemap;
--- app/src/js/index.js
+++ app/src/js/index.js
@@ -1,5 +1,6 @@
 import Dialog from './components/dialog';
 import Page from './components/page';
+import Sitemap from './components/sitemap';
 
 
 // add event listener
@@ -18,3 +19,21 @@ document.querySelector('.js--action-url')
       }
     });
   });
+
+  document.querySelector('.js--action-sitemap')
+    .addEventListener('click', () => {
+      const dialog = new Dialog('Enter an url of a sitemap', 'Submit Sitemap', {
+        async onSubmit(val,  input) {
+          let newSitemap;
+          try {
+            newSitemap = new Sitemap(val);
+            await newSitemap.render();
+            dialog.destroy();
+          } catch (e) {
+            console.log(e);
+            // there was an error while adding a page
+            input.value = ''; // reset input field
+          }
+        }
+      });
+    });
